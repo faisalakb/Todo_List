811eb790f3c12a6be6475a6dcbda2bcf138065f0
coker2019 <olabisi2k11@gmail.com>
2023-05-19 21:36:19 +0100

    Add test functions

--- src/index.test.js
+++ src/index.test.js
@@ -182,4 +182,12 @@ describe('Edit Item', () => {
 
     todoList.push(item1, item2, item3);
     display();
-   
+    window.addEventListener('DOMContentLoaded', () => {
+      const clearBtn = document.getElementById('clsBtn');
+      clearBtn.click();
+    });
+
+    const completedItemElements = document.querySelectorAll('.completed');
+    expect(completedItemElements.length).toBe(0);
+  });
+});
